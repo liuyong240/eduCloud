c9a6840f628b58d57336d90d9f75c9f063f02603
cloudfirst <4167023@qq.com>
2014-08-03 22:37:49 +0800

    login by auth & ajax POST is ready.

--- luhyacloud/clc/urls.py
+++ luhyacloud/clc/urls.py
@@ -5,8 +5,15 @@ urlpatterns = patterns('',
     #url(r'^main/top/$',     views.admin_top_view,   name='main_top_view'),
 
     # API 1.0
+    # 1. Creating users
+    # 2. Changing passwords
+    # 3.
 
 
     # Web Page
-    url(r'^login$',                          views.logon_view,               name='logon_view'),
+    url(r'^login$',         views.display_login_window,               name='logon_view'),
+    url(r'^user_login$',    views.user_login,                         name='user_login'),
+    url(r'^index$',         views.index_view,                         name='index_view'),
+
+
     )
--- luhyacloud/clc/views.py
+++ luhyacloud/clc/views.py
@@ -24,5 +24,36 @@ MAX_LOG_COUNT=10
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-def logon_view(request):
+def display_login_window(request):
     return render(request, 'clc/login.html', {})
+
+def user_login(request):
+    response = {}
+    username = request.POST['email']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            response['status'] = "SUCCESS"
+            response['url'] = "/clc/index"
+            return HttpResponse(json.dumps(response), mimetype='application/json')
+        else:
+            # Return a 'disabled account' error message
+            response['status'] = "FAILURE"
+            response['reason'] = "account is disabled"
+            return HttpResponse(json.dumps(response), mimetype='application/json')
+    else:
+        # Return an 'invalid login' error message.
+        response['status'] = "FAILURE"
+        response['reason'] = "account is invalid"
+        return HttpResponse(json.dumps(response), mimetype='application/json')
+
+def user_logout(request):
+    logout(request)
+    display_login_window(request)
+
+@login_required
+def index_view(request):
+    return render(request, 'clc/index.html', {})
+
