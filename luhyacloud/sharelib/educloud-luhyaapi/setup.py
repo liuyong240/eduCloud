dbd8f4de436a8076d197a164524b01a3bdd6d90c
Your Name <you@example.com>
2014-08-04 23:28:55 +0800

    change the name of package directory.

--- luhyacloud/sharelib/educloud-luhyaapi/setup.py
+++ luhyacloud/sharelib/educloud-luhyaapi/setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='luhyaapi',
-    version='0.1',
+    version='1.1',
     packages=['luhyaapi'],
     include_package_data=True,
     license='BSD License',  # example license
