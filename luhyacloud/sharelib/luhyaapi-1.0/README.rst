da6ee4427879043e12d15c365555b55b30738e10
Your Name <you@example.com>
2014-08-04 23:19:19 +0800

    create a pypi package for luhyaapi module. all other modules can be done same way for distribution.

---
+++ luhyacloud/sharelib/luhyaapi-1.0/README.rst
@@ -0,0 +1,7 @@
+=====================================================================
+ Python LuhyaAPI library 1.0
+=====================================================================
+run command to build source distribution that can be installed by pip.
+
+- python setup.py sdist
+
---
+++ luhyacloud/sharelib/luhyaapi-1.0/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='luhyaapi',
+    version='0.1',
+    packages=['luhyaapi'],
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='luhay cloud basic api library.',
+    long_description=README,
+    url='http://www.example.com/',
+    author='thomas.li',
+    author_email='4167023@qq.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
