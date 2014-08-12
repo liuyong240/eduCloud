14d6d751a5f43aa26ef597025ab33afcf3c5ac73
Your Name <you@example.com>
2014-08-12 22:01:34 +0800

    add a script to run sql script by bash.

---
+++ luhyacloud/clc/sql/init_data.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+mysql -uroot -proot mysql < ecauthpath.sql  
+mysql -uroot -proot mysql < ecostypes.sql  
+mysql -uroot -proot mysql < ecrbac.sql  
+mysql -uroot -proot mysql < ecserverrole.sql  
+mysql -uroot -proot mysql < ecvmtypes.sql  
+mysql -uroot -proot mysql < ecvmusages.sql
