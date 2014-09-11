e0d7bc611ad255d774dd01ff239dc4cefa7628e2
Your Name <you@example.com>
2014-09-12 06:45:22 +0800

    add missing db table into init data script.

--- luhyacloud/clc/sql/init_data.sh
+++ luhyacloud/clc/sql/init_data.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
-mysql -uroot -proot mysql < ecauthpath.sql  
-mysql -uroot -proot mysql < ecostypes.sql  
+mysql -uroot -proot mysql < ecauthpath.sql
+mysql -uroot -proot mysql < ecccresources.sql
+mysql -uroot -proot mysql < ecclusternetmode.sql
+mysql -uroot -proot mysql < ecostypes.sql
 mysql -uroot -proot mysql < ecrbac.sql  
-mysql -uroot -proot mysql < ecserverrole.sql  
+mysql -uroot -proot mysql < ecserverrole.sql
+mysql -uroot -proot mysql < ecservers.sql
 mysql -uroot -proot mysql < ecvmtypes.sql  
 mysql -uroot -proot mysql < ecvmusages.sql
-mysql -uroot -proot mysql < ecclusternetmode.sql
+
 
