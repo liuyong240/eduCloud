0276925d7401c5bed7246440208b52dad8d429e3
thomas.li <thomas.li>
2014-12-16 14:29:21 +0800

    remove delted db table in init_data.sh script.

--- luhyacloud/clc/sql/init_data.sh
+++ luhyacloud/clc/sql/init_data.sh
@@ -2,7 +2,6 @@
 mysql -uroot -proot mysql < ecauthpath.sql
 mysql -uroot -proot mysql < ecaccount.sql
 mysql -uroot -proot mysql < ecccresources.sql
-mysql -uroot -proot mysql < ecclusternetmode.sql
 mysql -uroot -proot mysql < ecostypes.sql
 mysql -uroot -proot mysql < ecserverrole.sql
 mysql -uroot -proot mysql < ecservers.sql
