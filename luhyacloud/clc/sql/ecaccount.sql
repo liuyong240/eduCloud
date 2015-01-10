2793977c33ed7ab7fe86933b6905e4d747163416
thomase-cloud <4167023@qq.com>
2015-01-10 15:14:05 +0800

    add description, vpara default value for clc_ecaccount.

--- luhyacloud/clc/sql/ecaccount.sql
+++ luhyacloud/clc/sql/ecaccount.sql
@@ -1,3 +1,3 @@
 alter table clc_ecaccount convert to character set utf8;
-INSERT INTO clc_ecaccount (`userid`, `showname`, `ec_authpath_name`, `phone`) VALUES ("luhya", "luhya", "云平台管理员", "911");
+INSERT INTO clc_ecaccount (`userid`, `showname`, `ec_authpath_name`, `phone`, `description`,`vdpara`) VALUES ("luhya", "luhya", "云平台管理员", "911", "cloud platform administrator", "{}");
 
