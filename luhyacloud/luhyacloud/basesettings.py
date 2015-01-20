14a082bd03887f7ed0721e47429cf105570de7d2
Your Name <you@example.com>
2015-01-21 07:00:45 +0800

    find root cause to switch between en-us and zh-CN

--- luhyacloud/luhyacloud/basesettings.py
+++ luhyacloud/luhyacloud/basesettings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
+    # 'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
