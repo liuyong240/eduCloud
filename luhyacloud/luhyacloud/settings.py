c3f8f98dac54903f65ab9aa56c8c3e55bd263bd4
Your Name <you@example.com>
2015-01-28 23:51:10 +0800

    first edition of portal index.html.

--- luhyacloud/luhyacloud/settings.py
+++ luhyacloud/luhyacloud/settings.py
@@ -6,15 +6,22 @@ import os
 if os.path.exists("/etc/educloud/modules/clc") == True:
     from clcsettings import *
     TEMPLATE_DIRS.append(os.path.join(BASE_DIR, 'portal', 'templates'))
-    TEMPLATE_DIRS.append(os.path.join(BASE_DIR, 'clc', 'templates'))
+    TEMPLATE_DIRS.append(os.path.join(BASE_DIR, 'clc',    'templates'))
 
     INSTALLED_APPS += (
+        'portal',
         'clc',
         'walrus',
     )
 
     STATICFILES_DIRS += (
+        os.path.join(BASE_DIR, 'portal', "static"),
         os.path.join(BASE_DIR, "clc", "static"),
+    )
+
+    LOCALE_PATHS += (
+        os.path.join(BASE_DIR, 'portal', 'conf/locale'),
+        os.path.join(BASE_DIR, 'clc',    'conf/locale'),
     )
 else:
     pass
