ec0a85e98c5b085f69c0605e1b9822b27105a58a
Your Name <you@example.com>
2015-02-17 08:19:36 +0800

    add java sdk definition.

--- webconfig/sdk/cloud-desktop/java/educloudsdk/src/vd/remote/rvdHttpClient.java
+++ webconfig/sdk/cloud-desktop/java/educloudsdk/src/vd/remote/rvdHttpClient.java
@@ -5,12 +5,10 @@
  */
 package vd.remote;
 
-import java.io.IOException;
 import org.apache.http.client.fluent.Form;
 import org.apache.http.client.fluent.Request;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 /**
  *
@@ -32,7 +30,6 @@ public class rvdHttpClient {
     private String run_url; 
     private String stop_url;
     private String vmstatus_url; 
-    private String remove_task_url; 
     private String rdp_url;
     private String del_vm_url;
     
@@ -52,7 +49,6 @@ public class rvdHttpClient {
         run_url         = "clc/api/1.0/rvd/run";
         stop_url        = "clc/api/1.0/rvd/stop";
         vmstatus_url    = "clc/api/1.0/rvd/getvmstatus";
-        remove_task_url = "clc/api/1.0/tasks/delete";
         rdp_url         = "clc/api/1.0/rvd/get_rdp_url";
         del_vm_url      = "clc/api/1.0/tasks/delete";
     }
@@ -67,11 +63,17 @@ public class rvdHttpClient {
         user_password   = pwd;
     }
     
+    /*
+    return : true or false
+    */
     public Boolean logon() {
         String url = String.format("http://%s:%d/%s", host_IP, host_port, login_url);
         
         try {
-            String myret = Request.Post(url).bodyForm(Form.form().add("email", user_id).add("password", user_password).build()).execute().returnContent().asString();
+            String myret = Request.Post(url).bodyForm(Form.form().
+                    add("email", user_id).
+                    add("password", user_password).build()).
+                    execute().returnContent().asString();
             JSONParser parser=new JSONParser();
             Object myobj = parser.parse(myret);
             JSONObject response = (JSONObject)myobj;
@@ -82,10 +84,324 @@ public class rvdHttpClient {
             } else {
                 return false;
             }
-            
         } catch (Exception pe) {
             return false;
         }
     }
-
+    
+    /*
+    # if sucess, return as belwo
+    # {
+    #   'Result' : 'OK',
+    #   'data'   : list of vms
+    # }
+    #
+    # each vm in list looks like  below:
+    # {
+    #   'ecid'      : 'image id',
+    #   'name'      : 'image name',
+    #   'ostype'    : 'image os type',
+    #   'desc'      : 'image description',
+    #   'tid'       : 'transaction id',
+    #   'phase'     : 'transaction phase',
+    #   'state'     : 'transaction state',
+    #   'mgr_url'   : 'RDP access ip:port'
+    #   'id'        : 'index in list'
+    # }
+    #
+    # if error, return as below
+    # {
+    #   'Result' : 'Failed',
+    #   'error'  : 'error message'
+    # }
+    */
+    public JSONObject getVDList() {
+        String url = String.format("http://%s:%d/%s", host_IP, host_port, list_vm);
+        
+        try {
+            String myret = Request.Post(url).bodyForm(Form.form().
+                    add("user", user_id).
+                    add("sid",  seesionID).
+                    build()).execute().returnContent().asString();
+            
+            JSONParser parser=new JSONParser();
+            Object myobj = parser.parse(myret);
+            JSONObject response = (JSONObject)myobj;
+            return response;
+        } catch (Exception pe) {
+            JSONObject except_ret = new JSONObject();
+            return except_ret;
+        }
+    }
+    
+    /*
+    ##
+    ## return :
+    ##   sucess :  ['Result': 'OK',    'tid'   : tid]
+    ##   failed :  ['Result': 'FAIL',  'error' : error msg]
+    ##
+    */
+    public JSONObject _create_tvd(JSONObject vmdata) {
+        String url = String.format("http://%s:%d/%s", host_IP, host_port, create_url);
+        
+        try {
+            String myret = Request.Post(url).bodyForm(Form.form().
+                    add("sid",  seesionID).
+                    build()).execute().returnContent().asString();
+            
+            JSONParser parser=new JSONParser();
+            Object myobj = parser.parse(myret);
+            JSONObject response = (JSONObject)myobj;
+            return response;
+        } catch (Exception pe) {
+            JSONObject except_ret = new JSONObject();
+            return except_ret;
+        }
+    }
+    
+    /*
+    ##
+    ## return :
+    ##   sucess :  ['Result': 'OK',    'tid'   : tid]
+    ##   failed :  ['Result': 'FAIL',  'error' : error msg]
+    ##
+    */
+    public JSONObject _start_tvd(JSONObject vmdata) {
+        String url = String.format("http://%s:%d/%s", host_IP, host_port, start_url);
+        
+        try {
+            String myret = Request.Post(url).bodyForm(Form.form().
+                    add("sid",  seesionID).
+                    build()).execute().returnContent().asString();
+            
+            JSONParser parser=new JSONParser();
+            Object myobj = parser.parse(myret);
+            JSONObject response = (JSONObject)myobj;
+            return response;
+        } catch (Exception pe) {
+            JSONObject except_ret = new JSONObject();
+            return except_ret;
+        }
+    }
+    
+    public String[]  _parseTID(String tid) {
+        return tid.split(":");
+    }
+    
+    public JSONObject startVM(JSONObject vmdata) {
+        String tid = (String)vmdata.get("tid");
+        if (tid.equals("")) {
+            return _create_tvd(vmdata);
+        } else {
+            return _start_tvd(vmdata);
+        }
+    }
+    
+    /*
+    ##
+    ## return :
+    ##   sucess :  ['Result': 'OK',    'tid'   : tid]
+    ##
+    */
+    public JSONObject stopVM(JSONObject vmdata) {
+        String tid = (String)vmdata.get("tid");
+        String[] tidArray = _parseTID(tid);
+        
+        String url = String.format("http://%s:%d/%s/%s/%s/%s", 
+                host_IP, host_port, stop_url, tidArray[0], tidArray[1], tidArray[2]);
+        
+        try {
+            String myret = Request.Post(url).bodyForm(Form.form().
+                    add("sid",  seesionID).
+                    build()).execute().returnContent().asString();
+            
+            JSONParser parser=new JSONParser();
+            Object myobj = parser.parse(myret);
+            JSONObject response = (JSONObject)myobj;
+            return response;
+        } catch (Exception pe) {
+            JSONObject except_ret = new JSONObject();
+            return except_ret;
+        }
+    }
+    
+    /*
+    ##
+    ## return :
+    ##   sucess :  ['Result': 'OK',    'tid'   : tid]
+    ##   failed :  ['Result': 'FAIL',  'error' : error msg]
+    ##
+    */
+    public JSONObject prepareVM(JSONObject vmdata) {
+        String tid = (String)vmdata.get("tid");
+        String[] tidArray = _parseTID(tid);
+        
+        String url = String.format("http://%s:%d/%s/%s/%s/%s", 
+                host_IP, host_port, prepare_url, tidArray[0], tidArray[1], tidArray[2]);
+        
+        try {
+            String myret = Request.Post(url).bodyForm(Form.form().
+                    add("sid",  seesionID).
+                    build()).execute().returnContent().asString();
+            
+            JSONParser parser=new JSONParser();
+            Object myobj = parser.parse(myret);
+            JSONObject response = (JSONObject)myobj;
+            return response;
+        } catch (Exception pe) {
+            JSONObject except_ret = new JSONObject();
+            return except_ret;
+        }
+    }
+    
+    /*
+    ##
+    ##  return = {
+    ##    'type': 'taskstatus',
+    ##    'phase': "preparing",
+    ##    'state': 'downloading',
+    ##    'progress': 0,
+    ##    'tid': tid,
+    ##    'prompt': '',
+    ##    'errormsg': '',
+    ##    'failed' : 0
+    ##  }
+    ##
+    */
+    public JSONObject getPrepareProgress(JSONObject vmdata) {
+        String tid = (String)vmdata.get("tid");
+        String[] tidArray = _parseTID(tid);
+        
+        String url = String.format("http://%s:%d/%s/%s/%s/%s", 
+                host_IP, host_port, progress_url, tidArray[0], tidArray[1], tidArray[2]);
+        
+        try {
+            String myret = Request.Post(url).bodyForm(Form.form().
+                    add("sid",  seesionID).
+                    build()).execute().returnContent().asString();
+            
+            JSONParser parser=new JSONParser();
+            Object myobj = parser.parse(myret);
+            JSONObject response = (JSONObject)myobj;
+            return response;
+        } catch (Exception pe) {
+            JSONObject except_ret = new JSONObject();
+            return except_ret;
+        }
+    }
+    
+    /*
+    ##
+    ## return :
+    ##   sucess :  ['Result': 'OK',    'tid'   : tid]
+    ##
+    */
+    public JSONObject runVM(JSONObject vmdata) {
+        String tid = (String)vmdata.get("tid");
+        String[] tidArray = _parseTID(tid);
+        
+        String url = String.format("http://%s:%d/%s/%s/%s/%s", 
+                host_IP, host_port, run_url, tidArray[0], tidArray[1], tidArray[2]);
+        
+        try {
+            String myret = Request.Post(url).bodyForm(Form.form().
+                    add("sid",  seesionID).
+                    build()).execute().returnContent().asString();
+            
+            JSONParser parser=new JSONParser();
+            Object myobj = parser.parse(myret);
+            JSONObject response = (JSONObject)myobj;
+            return response;
+        } catch (Exception pe) {
+            JSONObject except_ret = new JSONObject();
+            return except_ret;
+        }
+    }
+    
+    /*
+    ##
+    ##  return = {
+    ##        'type'    : 'taskstatus',
+    ##        'phase'  : "editing",
+    ##        'state'  : 'stopped', 'booting', 'running' ,
+    ##        'tid'    : _tid,
+    ##        'failed' : 0
+    ##  }
+    ##
+    */
+    public JSONObject getVMStatus(JSONObject vmdata) {
+        String tid = (String)vmdata.get("tid");
+        String[] tidArray = _parseTID(tid);
+        
+        String url = String.format("http://%s:%d/%s/%s/%s/%s", 
+                host_IP, host_port, vmstatus_url, tidArray[0], tidArray[1], tidArray[2]);
+        
+        try {
+            String myret = Request.Post(url).bodyForm(Form.form().
+                    add("sid",  seesionID).
+                    build()).execute().returnContent().asString();
+            
+            JSONParser parser=new JSONParser();
+            Object myobj = parser.parse(myret);
+            JSONObject response = (JSONObject)myobj;
+            return response;
+        } catch (Exception pe) {
+            JSONObject except_ret = new JSONObject();
+            return except_ret;
+        }
+    }
+    
+    /*
+    ##
+    ## return :
+    ##   sucess :  ['Result': 'OK',    'mgr_url'   : rdp url]
+    ##
+    */
+    public JSONObject getRDPUrl(JSONObject vmdata) {
+        String tid = (String)vmdata.get("tid");
+        String[] tidArray = _parseTID(tid);
+        
+        String url = String.format("http://%s:%d/%s/%s/%s/%s", 
+                host_IP, host_port, rdp_url, tidArray[0], tidArray[1], tidArray[2]);
+        
+        try {
+            String myret = Request.Post(url).bodyForm(Form.form().
+                    add("sid",  seesionID).
+                    build()).execute().returnContent().asString();
+            
+            JSONParser parser=new JSONParser();
+            Object myobj = parser.parse(myret);
+            JSONObject response = (JSONObject)myobj;
+            return response;
+        } catch (Exception pe) {
+            JSONObject except_ret = new JSONObject();
+            return except_ret;
+        }
+    }
+    
+    public JSONObject delet_vm(JSONObject vmdata) {
+        String tid = (String)vmdata.get("tid");
+        String[] tidArray = _parseTID(tid);
+        
+        String url = String.format("http://%s:%d/%s/%s/%s/%s", 
+                host_IP, host_port, del_vm_url, tidArray[0], tidArray[1], tidArray[2]);
+        
+        try {
+            String myret = Request.Post(url).bodyForm(Form.form().
+                    add("tid",  tid).
+                    build()).execute().returnContent().asString();
+            
+            JSONParser parser=new JSONParser();
+            Object myobj = parser.parse(myret);
+            JSONObject response = (JSONObject)myobj;
+            return response;
+        } catch (Exception pe) {
+            JSONObject except_ret = new JSONObject();
+            return except_ret;
+        }
+    }
+    
+    public JSONObject errorHandle(JSONObject vmdata) {
+        return delet_vm(vmdata);
+    }
 }
--- webconfig/sdk/cloud-desktop/java/sdkSample/src/sdksample/SdkSample.java
+++ webconfig/sdk/cloud-desktop/java/sdkSample/src/sdksample/SdkSample.java
@@ -5,14 +5,38 @@
  */
 package sdksample;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import vd.remote.rvdHttpClient;
 
 public class SdkSample {
     public static void main(String[] args) {
         rvdHttpClient hc = new rvdHttpClient();
-        hc.setHost("192.168.96.122", 8000);
+        //hc.setHost("192.168.96.122", 8000);
+        hc.setHost("10.0.0.21", 8000);
         hc.setUser("wangfeng", "11111111");
+        
         Boolean ret = hc.logon();
-        return;
+        
+        if (ret == false) {
+            System.out.println("logon failed, try correct user name & password");
+            System.exit(-1);
+        }
+        
+        JSONObject list_vms;
+        list_vms = hc.getVDList();
+        JSONArray  array_vms = (JSONArray)list_vms.get("data");
+        
+        Object vmobj = array_vms.get(0);
+        String phase = (String)((JSONObject)vmobj).get("phase");
+        String state = (String)((JSONObject)vmobj).get("state");
+        
+        if (phase.equals("editing")) {
+            if (state.equals("running") || state.equals("Running")) {
+               
+            }
+        }
+         
     }
 }
+
