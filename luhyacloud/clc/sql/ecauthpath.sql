3a14cc2aaf54e61c5678767ca51a7b2542363674
thomas lee <4167023@qq.com>
2015-09-01 23:16:34 +0800

    add a role for teacher / employee.

--- luhyacloud/clc/sql/ecauthpath.sql
+++ luhyacloud/clc/sql/ecauthpath.sql
@@ -1,3 +1,4 @@
 alter table clc_ecauthpath convert to character set utf8;
 INSERT INTO clc_ecauthpath (`ec_authpath_name`, `ec_authpath_value`) VALUES ("云平台管理员", "eduCloud.admin");
-INSERT INTO clc_ecauthpath (`ec_authpath_name`, `ec_authpath_value`) VALUES ("龙游教育局管理员", "eduCloud.longyou.admin");
+INSERT INTO clc_ecauthpath (`ec_authpath_name`, `ec_authpath_value`) VALUES ("教育局管理员", "eduCloud.edu-depart.admin");
+INSERT INTO clc_ecauthpath (`ec_authpath_name`, `ec_authpath_value`) VALUES ("教育局员工",   "eduCloud.edu-depart.employee");
